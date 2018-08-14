973b99f7872619eb05fc95d052cf14515a875d30
yuichiro toyama <ytoyama1872@gmail.com>
2018-08-14 14:37:26 +0100

    task manager working

--- db/migrate/20180814103542_create_tasks.rb
+++ db/migrate/20180814103542_create_tasks.rb
@@ -1,4 +1,4 @@
-raisclass CreateTasks < ActiveRecord::Migration[5.2]
+class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_14_103542) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
